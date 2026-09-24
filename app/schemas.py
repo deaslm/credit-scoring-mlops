96bb2569165a927768fff105ee7b44bc40b22fc4
deaslm <selimidea@hotmail.com>
2026-09-25 01:51:59 +0200

    feat: ajout dans schemas pydantic d'un exemple pre-rempli dans Swagger pour faciliter les demonstrations

--- app/schemas.py
+++ app/schemas.py
@@ -13,6 +13,18 @@ class PredictionRequest(BaseModel):
     sk_id_curr: Optional[int] = None                     # ce champs peut etre soit un entier soit vide si on veut juste calculer un score sans id.
     features: Dict[str, float]                           # la clé doit etre du texte(nom feature) et la valeur un nombre
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "sk_id_curr": 100002,
+                "features": {                           # exemple de dictionnaire avec les features
+                    "AMT_INCOME_TOTAL": 200000,
+                    "AMT_CREDIT": 40000
+                }
+            }
+        }
+    }
+
 
 class PredictionResponse(BaseModel):
     """Reponse renvoyee par l'API apres scoring."""
